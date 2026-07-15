0e3de7615485c1e140381214347a55c71d6eebd0
John Reynolds <reynolds@ucalgary.ca>
2026-07-15 12:54:34 -0700

    delete

--- serve.py
+++
@@ -1,6 +0,0 @@
-from http.server import ThreadingHTTPServer,SimpleHTTPRequestHandler
-from pathlib import Path
-import os
-os.chdir(Path(__file__).parent)
-print('Open http://localhost:8000')
-ThreadingHTTPServer(('0.0.0.0',8000),SimpleHTTPRequestHandler).serve_forever()
